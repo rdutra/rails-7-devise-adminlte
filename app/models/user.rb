ed8635b6069af5593466bb2adba1a85c1ddbbb4d
Rodrigo Dutra <dutraok@gmail.com>
2022-08-23 15:11:14 -0300

    added roles to user, renamed home to dashboard;

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  enum role: {admin: 0, manager: 1, operator: 2}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+  
 end
